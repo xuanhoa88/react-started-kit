c813ef7c25fdc7ca69d04eaad350b913b950d5c2
XuaNguyen <xuan.0211@gmail.com>
2017-12-14 02:59:40 +0700

    Initial

---
+++ src/config.js
@@ -0,0 +1,51 @@
+/**
+ * React Starter Kit (https://www.reactstarterkit.com/)
+ *
+ * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+/* eslint-disable max-len */
+
+if (process.env.BROWSER) {
+  throw new Error(
+    'Do not import `config.js` from inside the client-side code.',
+  );
+}
+
+module.exports = {
+  // Node.js app
+  port: process.env.PORT || 3000,
+
+  // API Gateway
+  api: {
+    // API URL to be used in the client-side code
+    clientUrl: process.env.API_CLIENT_URL || '',
+    // API URL to be used in the server-side code
+    serverUrl:
+      process.env.API_SERVER_URL ||
+      `http://localhost:${process.env.PORT || 3000}`,
+  },
+
+  // Web analytics
+  analytics: {
+    // https://analytics.google.com/
+    googleTrackingId: process.env.GOOGLE_TRACKING_ID, // UA-XXXXX-X
+  },
+
+  // Authentication
+  auth: {
+    jwt: { secret: process.env.JWT_SECRET || 'React Starter Kit' },
+
+    // https://cloud.google.com/console/project
+    google: {
+      id:
+        process.env.GOOGLE_CLIENT_ID ||
+        '251410730550-ahcg0ou5mgfhl8hlui1urru7jn5s12km.apps.googleusercontent.com',
+      secret: process.env.GOOGLE_CLIENT_SECRET || 'Y8yR9yZAhm9jQ8FKAL8QIEcd',
+    }
+
+  },
+};
